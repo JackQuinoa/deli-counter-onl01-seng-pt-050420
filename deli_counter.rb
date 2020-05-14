b0c675d654338a37cc7c013d822d68df62293075
Jack Quinoa <jquinoa208@gmail.com>
2020-05-14 23:07:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else 
+    current_line = "The line is currently:"
+    katz_deli.each_with_index do |name , number|
+      current_line << " #{number + 1}. #{name}"
+    end
+    puts current_line
+  end
+end
+
+def take_a_number(katz_deli, new_customer)
+  katz_deli << new_customer
+  
+end
